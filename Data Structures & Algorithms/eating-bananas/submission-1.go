4d57977c343ac61ade5ad60e81a1356ddc397069
Adick Rincones <rinconesadick@gmail.com>
2026-09-08 11:36:33 -0400

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.go
@@ -0,0 +1,30 @@
+func minEatingSpeed(piles []int, h int) int {
+	maxPile := 0
+	for _, p := range piles {
+		maxPile = max(p, maxPile)
+	}
+
+	l, r := 1, maxPile
+
+	for l < r {
+		k := l + (r-l)/2
+
+		if canFinish(piles, h, k) {
+			r = k
+		} else {
+			l = k + 1
+		}
+	}
+
+	return l
+}
+
+func canFinish(piles []int, h int, k int) bool {
+	hours := 0
+
+	for _, p := range piles {
+		hours += (p + k - 1) / k
+	}
+
+	return hours <= h
+}
